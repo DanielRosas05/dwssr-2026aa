da25331469fdb6fa14e971fead7c3364c048032f
DanielRosas05 <159564415+DanielRosas05@users.noreply.github.com>
2026-03-25 16:55:54 +0000

    chore:🔧 Vite Configuration En proceso...

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+// Importo la funcion de conguracion de Vite
+import { defineConfig } from 'vite';
+//Importo un resolvedor de rutas
+import { resolve } from 'node:path';
+// Exporto la instancia de la configuracion
+export default defineConfig({})
