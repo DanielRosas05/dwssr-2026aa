d7a0b49669962beff78e5d0cd2227160c8cd282a
DanielRosas05 <159564415+DanielRosas05@users.noreply.github.com>
2026-04-29 15:22:36 +0000

    feat:instalación de Tailwind CSS y DaisyUI

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
-//
+// Importo la duncion de configuracion de Vite
 import { defineConfig } from 'vite';
+// Importo un resolvedor de rutas
 import { resolve } from 'path';
-
+// Importando TAILWINDCSS
+import tailwindcss from 'tailwindcss';
 export default defineConfig({
   root: 'src',
+  // plugins: [tailwindcss()]
+  plugins : [tailwindcss()],
 
   server: {
     port: 5173,
